a537d681a93b4ae39c11ada4bbc835f3d745a414
bfuentes11 <70171808+bfuentes11@users.noreply.github.com>
2023-05-23 00:05:04 -0500

    Created player and added displays of strength, name, and health.

--- ConsoleApplication1/Powerplay1.cpp
+++ ConsoleApplication1/Powerplay1.cpp
@@ -3,22 +3,69 @@
 #include <random>
 using namespace std;
 
+// Player template
 struct Player
 {
 	std::string Name;
-	int strength{ 0 };
-	int health{ 0 };
+	int strength{};
+	int health{};
 };
+
+//  Displays character name
+void nameCharacter(Player& player) {
+	std::cout << "Name: " << player.Name << std::endl;
+}
+// Displays character strength
+void displayStrength(Player& player) {
+	std::cout << "Strength: " << player.strength<< std::endl;
+}
+
+void displayHealth(Player& player) {
+	std::cout << "Health: " << player.health << std::endl;
+}
 
 int main()
 {
-	random_device seed;
-	default_random_engine e(seed());
+	//Engine, Player, and Monster setup
+
+	std::random_device seed;
+	std::default_random_engine e(seed());
 	std::normal_distribution<double> nDist(30.0, 5.0);
+	std::uniform_int_distribution<int> uiDist(1, 3);
 
-	double x = nDist(e);
+	int strPlayer = 1 + nDist(e);
+	int vitPlayer = 1 + nDist(e);
 
-	cout << x;
+	int strMonster = 1 + nDist(e);
+	int vitMonster = 1 + nDist(e);
+	
+	std::vector <int> monst;
+	monst = { uiDist(e) };
+	
+	
+	
+	string customName;
+
+	std::cout << "What is your name" << endl;
+	
+	std::cin >> customName;
+
+	cout << endl;
+
+	std::cout << "Hello " << customName << endl;
+	
+	Player Link{ customName , strPlayer, vitPlayer};
+	cout << endl << "Here is your profile: " << endl;
+	nameCharacter(Link);
+	displayHealth(Link);
+	displayStrength(Link);
+
+	cout << monst.size();
+
+	//Game Start
+
+
+
 	
 	
 	
