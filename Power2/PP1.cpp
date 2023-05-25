316517f475bbc1ab33b6f7db894d1b22464aec93
bfuentes11 <70171808+bfuentes11@users.noreply.github.com>
2023-05-25 13:23:56 -0500

    all functions completed numMonsters on line 52 changed since int monsters is now in our function outside of main()

--- Power2/PP1.cpp
+++ Power2/PP1.cpp
@@ -20,21 +20,10 @@ std::vector<Object>	loadMonsters(const std::string& fileName);
 void displayBattle(const Object& player, const std::vector<Object>& monsters);
 bool monsterAttack(Object& player, std::vector<Object>& monsters);
 int playerAttack(Object& player,const std::vector<Object>& monsters);
-int attack();
-void defend();
+int attack(Object& object);
+void defend(Object& object, int damage);
 void heal();
 
-
-//TodoList
-/*
-loadmonsters DONE
-displaybattle DONE
-monsterattack
-playerattack
-attack
-defend
-heal
-*/
 std::random_device seed;
 std::default_random_engine e(seed());
 
@@ -46,7 +35,7 @@ int main()
 	std::normal_distribution<double> randomHealth(30.0, 5.0);
 	std::normal_distribution<double> randomStrength(5.0, 1.0);
 
-	int numMonsters;
+	
 
 	Object player
 	{
@@ -55,35 +44,62 @@ int main()
 		std::max(1, (int)randomHealth(e))
 	};
 
-	std::vector<Object>monsters{loadMonsters("monsters.txt")};
+	std::vector<Object>monsters{ loadMonsters("monsters.txt") };
 
 	//Got rid of while loop temporarily, parts of it are being sent to our functions
 
 
-	std::cout << numMonsters << " monster(s) approaches!!" << std::endl;
+	std::cout << monsters.size() << " monster(s) approaches!!" << std::endl;
 	bool allDead{ false };
 
+	while (player.health > 0 && !monsterAttack(player, monsters)) {
 
 
-	if (player.health <= 0)
-	{
+		displayBattle(player, monsters);
+		std::cout << "What do you do? (a)ttack (h)eal ";
+		char command{  };
+		std::cin >> command;
+		switch (command)
+		{
+		case 'a':
+		{
+			std::cout << "Which Monster: ";
+			int monsterNum{ 0 };
+			std::cin >> monsterNum;
+			if (monsterNum > 0 && monsterNum <= monsters.size())
+			{
+				if (monsters.at(monsterNum - 1).health > 0)
+					monsters[monsterNum - 1].health -= player.strength;
+			}
+			break;
+		}
+		case 'h':
+			player.health += player.strength * 2;
+			break;
+		default:
+			std::cout << "please enter a or h" << std::endl;
+			break;
+		}
+	}
+
+	if (player.health <= 0) {
 		std::cout << "You Have Died" << std::endl;
 	}
-	if (player.health <= 0 && allDead)
-	{
-		std::cout << "BUT" << std::endl;
-	}
-	if (allDead)
+	else
 	{
 		std::cout << "You have killed the monsters!!!" << std::endl;
 	}
 	system("PAUSE");
+}
 
-	std::vector<Object> loadMonsters(const std::string & fileName) {
+
+	std::vector<Object> loadMonsters(const std::string& fileName)
+	{
 		std::fstream doc;
 		doc.open(fileName);
 		std::vector<Object> monsters;
 		if (doc.is_open()) {
+			int numMonsters;
 			doc >> numMonsters;
 			//We recreate the for loop so now it works with our document
 			for (int i = 0; i < numMonsters; i++) {
@@ -104,7 +120,7 @@ int main()
 		return monsters;
 	}
 
-	void displayBattle(Object& player, std::vector<Object>&monsters) {
+	void displayBattle(Object & player, std::vector<Object>&monsters) {
 		std::cout << player.name << ": " << player.health << std::endl
 			<< "  Monsters: " << std::endl;
 		for (int i{ 0 }; i < monsters.size(); i++)
@@ -117,8 +133,8 @@ int main()
 		}
 	}
 
-	bool monsterAttack(Object& player, std::vector<Object>& monsters) {
-		
+	bool monsterAttack(Object & player, std::vector<Object>&monsters) {
+
 		std::bernoulli_distribution willAttack(.75);
 		bool allDead{ true };
 		for (const auto& monster : monsters)
@@ -140,26 +156,31 @@ int main()
 		return allDead;
 	}
 
-	bool playerAttack() {
+	bool playerAttack(Object& player , std::vector<Object>& monsters) {
 		std::cout << "Which Monster: ";
 		int monsterNum{ 0 };
 		std::cin >> monsterNum;
-		if (monsterNum > 0 && monsterNum <= monsters.size())
-		{
-			defend(monsters[monsterNum -1], attack(player));
+		if (monsterNum > 0 && monsterNum <= monsters.size())		{
+			defend(monsters[monsterNum - 1], attack(player));
 		}
 	}
 
 	//attack defend work together
-	int attack(Object& object) {
+	int attack(Object & object) {
 		std::normal_distribution<int> damageDealt(object.strength * 2);
 		return std::max(1, (int)damageDealt(e));
 		std::cout << object.name << "deals ";
 	}
 
-	void defend(Object& object, int damage) {
+	void defend(Object & object, int damage) {
 		std::cout << damage << " damage to " << object.name << "!!" << std::endl;
 		object.health = object.health - damage;
 	}
 
+	void heal(Object& object) {
+		std::normal_distribution<int> lifeHeal(object.strength * 2, 3);
+		int healAmount{ std::max(1, lifeHeal(e)) };
+		std::cout << "You have restored  " << healAmount << " life points." << std::endl;
+		object.health = object.health + healAmount;
+	}
 }
