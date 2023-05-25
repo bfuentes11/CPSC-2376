52c181e5fca78b08f3dfd9a6a616800ff774022b
bfuentes11 <70171808+bfuentes11@users.noreply.github.com>
2023-05-25 12:45:57 -0500

    playerAttack almost finished, will be done once defend is implemented

--- Power2/PP1.cpp
+++ Power2/PP1.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <fstream>
 
 //Changed to PP1 Solution as recommended.
 
@@ -13,11 +14,12 @@ struct Object
 	int strength{ 0 };
 	int health{ 0 };
 };
-std::vector<Object>	loadMonsters(std::string& filename);
-//displayBattle is void because it does not return anything
+
+//Declarations
+std::vector<Object>	loadMonsters(const std::string& fileName);
 void displayBattle(const Object& player, const std::vector<Object>& monsters);
-int monsterAttack();
-int playerAttack();
+bool monsterAttack(Object& player, std::vector<Object>& monsters);
+int playerAttack(Object& player,const std::vector<Object>& monsters);
 int attack();
 void defend();
 void heal();
@@ -26,7 +28,7 @@ void heal();
 //TodoList
 /*
 loadmonsters DONE
-displaybattle
+displaybattle DONE
 monsterattack
 playerattack
 attack
@@ -37,12 +39,14 @@ std::random_device seed;
 std::default_random_engine engine(seed());
 
 
-int main()	
+int main()
 {
 
-	
+
 	std::normal_distribution<double> randomHealth(30.0, 5.0);
 	std::normal_distribution<double> randomStrength(5.0, 1.0);
+
+	int numMonsters;
 
 	Object player
 	{
@@ -50,15 +54,16 @@ int main()
 		std::max(1, (int)randomStrength(engine)),
 		std::max(1, (int)randomHealth(engine))
 	};
-	std::vector<Object>monsters{ loadMonsters(monsters.txt) };
 
-	//Got rid of for loop and engine for monsters 
-	//because our Monsters come from a text file now
-	
+	std::vector<Object>monsters{loadMonsters("monsters.txt")};
+
+	//Got rid of while loop temporarily, parts of it are being sent to our functions
+
 
 	std::cout << numMonsters << " monster(s) approaches!!" << std::endl;
 	bool allDead{ false };
-	
+
+
 
 	if (player.health <= 0)
 	{
@@ -73,4 +78,74 @@ int main()
 		std::cout << "You have killed the monsters!!!" << std::endl;
 	}
 	system("PAUSE");
+
+	std::vector<Object> loadMonsters(const std::string & fileName) {
+		std::fstream doc;
+		doc.open(fileName);
+		std::vector<Object> monsters;
+		if (doc.is_open()) {
+			doc >> numMonsters;
+			//We recreate the for loop so now it works with our document
+			for (int i = 0; i < numMonsters; i++) {
+
+				//Monster stats
+				std::string name;
+				int strength;
+				int health;
+				doc >> name;
+				doc >> strength;
+				doc >> health;
+
+				monsters.push_back({ name, strength,health });
+			}
+
+		}
+		doc.close();
+		return monsters;
+	}
+
+	void displayBattle(Object& player, std::vector<Object>&monsters) {
+		std::cout << player.name << ": " << player.health << std::endl
+			<< "  Monsters: " << std::endl;
+		for (int i{ 0 }; i < monsters.size(); i++)
+		{
+			std::cout << "   " << i + 1 << ". " << monsters[i].name << ": ";
+			if (monsters.at(i).health <= 0)
+				std::cout << "<DEAD> " << std::endl;
+			else
+				std::cout << monsters[i].health << std::endl;
+		}
+	}
+
+	bool monsterAttack(Object& player, std::vector<Object>& monsters) {
+		std::bernoulli_distribution willAttack(.75);
+		allDead = true;
+		for (const auto& monster : monsters)
+		{
+			if (monster.health > 0)
+			{
+				allDead = false;
+				if (willAttack(engine))
+				{
+					std::cout << monster.name << " attacks!" << std::endl;
+					player.health -= monster.strength;
+				}
+				else
+				{
+					std::cout << monster.name << " twiddles its thumbs" << std::endl;
+				}
+			}
+		}
+	}
+
+	bool playerAttack() {
+		std::cout << "Which Monster: ";
+		int monsterNum{ 0 };
+		std::cin >> monsterNum;
+		if (monsterNum > 0 && monsterNum <= monsters.size())
+		{
+			defend();
+		}
+	}
+
 }
